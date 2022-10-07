3342cbb5f144cc6a60c19ef04077e64850efe3da
Mariana Mena <maranashokry@ahoo.com>
2022-10-06 21:42:59 -0500

    modified:   index.html 	modified:   scripts/mariana.js 	modified:   scripts/mariana_main.js 	modified:   styles/main.css

--- scripts/mariana.js
+++ scripts/mariana.js
@@ -1,22 +1,23 @@
 import { fetchCats } from "./mariana_main.js";
 import { fetchDogs } from "./mariana_main.js";
+//import { addWinner } from "./mariana_main.js";
 /* -----------------Cats--------------------- */
-const displayCats = async () => {
+export const displayCats = async () => {
     const cats = await fetchCats();
+    //URLID(cats)
     renderCats(cats);
-};
-
+}
 let renderCats = (cats) => {
     console.log(cats);
     let html = ``;
-    html = 
-    `<img class="image" src= ${cats[0].url} alt="">`;
+    html = `<img class="image" src= ${cats[0].url} alt="">`;
     document.getElementById("m1").innerHTML = html;
 };
 displayCats();
 
+
 /* -----------------Dogs--------------------- */
-const displayDogs = async () => {
+export const displayDogs = async () => {
     const dogs = await fetchDogs();
     renderDogs(dogs);
 };
@@ -29,3 +30,35 @@ let renderDogs = (dogs) => {
     document.getElementById("m2").innerHTML = html;
 };
 displayDogs();
+
+/*---- Event ---*/
+// document.addEventListener("click", (e) => {
+//     //const [type, value] = event.target.value;
+//     let TYPE = "";
+//     let VOTE = "";
+//     let URL = "";
+//     let ID = ""; 
+//     if (e.target.id === "m1") {
+//         URL = `${cats[0].url}`;
+//         ID = `${cats[0].id}`;
+//     } else if (e.target.id === "m2") {
+//         URL = `${dogs[0].url}`;
+//         ID = `${dogs[0].id}`;
+//     }
+//         const NewWinner = {
+//             url: URL,
+//             type: TYPE,
+//             votes: VOTE,
+//             id: ID,
+//         };
+//     addWinner(NewWinner);
+    
+    
+//     const displayNewWinner = () => {
+//         const NewWinner = addWinner();
+//         renderWinner(NewWinner);
+//     };
+//     displayNewWinner();    
+// };
+
+// let newWinner = document.getElementById("m_displayWinner");
