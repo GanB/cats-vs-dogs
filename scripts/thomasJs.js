7eec8d06993d84a087fb07e2cf4775b28cf1b3af
Thomas Kibby <htkibby@gmail.com>
2022-10-10 10:35:28 -0500

    further button functionality

--- scripts/thomasJs.js
+++ scripts/thomasJs.js
@@ -7,9 +7,9 @@ const fetchCatThomas = async () => {
    return applicationState.apiCat
 }
 
-// const mapCats = () => {
-//    return applicationState.apiCat.map(cat =>({...cat}))
-// }
+const mapCats = () => {
+   return applicationState.apiCat.map(cat =>({...cat}))
+}
 
 const fetchDogThomas = async () => {
    const data = await fetch(apiDog)
@@ -18,18 +18,20 @@ const fetchDogThomas = async () => {
    return applicationState.apiDog
 }
 
-// const mapDogs = () => {
-//    return applicationState.apiDog.map(dog =>({...dog}))
-// }
+const mapDogs = () => {
+   return applicationState.apiDog.map(dog =>({...dog}))
+}
 
 const renderHTML = async () => {
    const cat = await fetchCatThomas()
    const dog = await fetchDogThomas()
    for (const cats of cat) {
       document.getElementById("timg1").innerHTML = `<img src="${cats.url}" alt="Image of a very cool cat" class="img">`;
+      applicationState.apiCat = cats.url
    }
    for (const dogs of dog) {
-      document.getElementById("timg2").innerHTML = `<img src="${dogs.url}" alt="Image of a very cool dog" class="img">`
+      document.getElementById("timg2").innerHTML = `<img src="${dogs.url}" alt="Image of a very cool dog" class="img">`;
+      applicationState.apiDog = dogs.url
    }
 }
 
@@ -37,9 +39,11 @@ renderHTML()
 
 document.addEventListener("click", e => {
    if (e.target.id === "voteCatT") {
+      document.getElementById("m_displayWinner").innerHTML += `<img src="${applicationState.apiCat}" class="img">`;
       renderHTML()
    }
    if (e.target.id === "voteDogT") {
+      document.getElementById("m_displayWinner").innerHTML += `<img src="${applicationState.apiDog}" class="img">`;
       renderHTML()
    }
 })
