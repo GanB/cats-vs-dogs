68b16edc6327d65e6349f52091c3a71b1a56d15c
Ganesh Babu <6440594+GanB@users.noreply.github.com>
2022-10-10 21:15:13 -0400

    ganesh section updates

--- scripts/ganesh/ganeshSection.js
+++ scripts/ganesh/ganeshSection.js
@@ -1,22 +1,29 @@
-import { getRandomCat, getRandomDog } from "./ganeshSectionDataAccess.js";
+import {
+  getRandomCat,
+  getRandomDog,
+  recordVote,
+  getWinners,
+} from "./ganeshSectionDataAccess.js";
 
 const ganeshSection = document.querySelector("#ganesh__section");
+const sidebar = document.querySelector("#sidebar");
 
 const renderGaneshSectionHtml = async () => {
   const randomCat = await getRandomCat();
   const randomDog = await getRandomDog();
 
-  console.log(randomCat[0].url);
-  console.log(randomDog[0].url);
+  //   console.log(randomCat[0].url);
+  //   console.log(randomDog[0].url);
 
   const ganeshSectionHtml = ` 
-        <div class="ganesh__section__parent__container">       
+        <div id="ganesh__section__parent__container" class="ganesh__section__parent__container">       
         <div class="ganesh__section__header">
           <h3 class="teamName">Ganesh's Section</h3>
         </div>
         <div id="ganesh" class="ganesh__section__image__parent">
           <div class="ganesh__section__image_container">
             <img
+                id="ganesh__section__image__clicked__cat__${randomCat[0].url}"
                 class="ganesh__section__image"
                 title="Click to Vote"
                 src=${randomCat[0].url} 
@@ -25,6 +32,7 @@ const renderGaneshSectionHtml = async () => {
           </div>
           <div class="ganesh__section__image_container">
             <img
+                id="ganesh__section__image__clicked__dog__${randomDog[0].url}"
                 class="ganesh__section__image"
                 title="Click to Vote"
                 src=${randomDog[0].url} 
@@ -39,3 +47,27 @@ const renderGaneshSectionHtml = async () => {
 };
 
 renderGaneshSectionHtml();
+
+const renderSidebar = async () => {
+  const winners = await getWinners();
+  console.log(winners);
+  const sidebarHtml = ``;
+  sidebar.innerHTML = sidebarHtml;
+};
+
+renderSidebar();
+
+ganeshSection.addEventListener("click", async (imageClicked) => {
+  if (imageClicked.target.id.startsWith("ganesh__section__image__clicked__")) {
+    const clickedId = imageClicked.target.id.split("__");
+    const url = clickedId.pop();
+    const type = clickedId.pop();
+
+    await recordVote({ url: url, type: type, vote: 1 });
+  }
+});
+
+ganeshSection.addEventListener("voteRecorded", (customEvent) => {
+  console.log("vote recorded");
+  renderGaneshSectionHtml();
+});
