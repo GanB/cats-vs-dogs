06c533056c62ed7129b66924c9676cf5eaf07ec6
Mariana Mena <maranashokry@ahoo.com>
2022-10-12 15:33:23 -0500

    Changed winner display

--- scripts/mariana.js
+++ scripts/mariana.js
@@ -1,9 +1,9 @@
 import { fetchCats } from "./mariana_main.js";
-import { fetchWCats } from "./mariana_main.js";
+//import { fetchWCats } from "./mariana_main.js";
 import { fetchDogs } from "./mariana_main.js";
-import { fetchWDogs } from "./mariana_main.js";
+//import { fetchWDogs } from "./mariana_main.js";
 import { addWinner } from "./mariana_main.js";
-//import { renderSidebar } from "../sidebar.js";
+import { renderSidebar } from "./sidebar.js";
 /* ------------------------------------------ */
 // mainContainer.addEventListener("click", clickEvent => {
 //     const ethanImgContainer = document.querySelector("#ethan-winner")
@@ -42,7 +42,7 @@ const displayCats = async () => {
 
 // const renderCats = (cats) => {
   let html = ``;
-  html = `<img id="M1" class="image" src= ${cats[0].url} alt="">`;
+  html = `<img id="M1" class="image" src= ${cats[0].url} alt="" title="Meow Meow">`;
   document.getElementById("m1").innerHTML = html;
   console.log(cats);
   document.addEventListener("click", (e) => {
@@ -65,28 +65,28 @@ const displayCats = async () => {
 };
 displayCats();
 /*-----Winner--cats------*/
-export const displayWCats = async (winner) => {
-  const cats = await fetchWCats();
-//   renderWCats(cats);
+// export const displayWCats = async (winner) => {
+//   const cats = await fetchWCats();
+// //   renderWCats(cats);
+// // };
+
+
+// // const renderWCats = (cats) => {
+//   let html = ``;
+//   let catsVote = 0;
+//   for (const cat of cats) {
+//     console.log(cats);
+//     html += `<img id="M1" class="image" src= ${cat.url} alt="">`;
+//     if (cat.type === "cat") {
+//       catsVote++;
+//     }
+//     let htmlM = ``;
+//     htmlM += `<div style="color:#7A0144;font-size:50px">${catsVote}</div>`;
+//     document.getElementById("Mcat").innerHTML = htmlM;
+//   }
+//   document.getElementById("sidebar_Mariana").innerHTML = html;
 // };
-
-
-// const renderWCats = (cats) => {
-  let html = ``;
-  let catsVote = 0;
-  for (const cat of cats) {
-    console.log(cats);
-    html += `<img id="M1" class="image" src= ${cat.url} alt="">`;
-    if (cat.type === "cat") {
-      catsVote++;
-    }
-    let htmlM = ``;
-    htmlM += `<div style="color:#7A0144;font-size:50px">${catsVote}</div>`;
-    document.getElementById("Mcat").innerHTML = htmlM;
-  }
-  document.getElementById("sidebar_Mariana").innerHTML = html;
-};
-displayWCats();
+// displayWCats();
 /* -----------------Dogs--------------------- */
 /* --fetch--display--render-- */
 
@@ -108,7 +108,7 @@ const displayDogs = async () => {
 // const renderDogs = (dogs) => {
   let html = ``;
   html = `
-    <img id="M2" class="image" src= ${dogs[0].url} alt="">`;
+    <img id="M2" class="image" src= ${dogs[0].url} alt="" title="Woof Woof">`;
   document.getElementById("m2").innerHTML = html;
   console.log(dogs);
   document.addEventListener("click", (e) => {
@@ -131,25 +131,25 @@ const displayDogs = async () => {
 };
 displayDogs();
 /*-----Winner--dogs------*/
-export const displayWDogs = async (winner) => {
-  const dogs = await fetchWDogs();
-//   renderWDogs(dogs);
+// export const displayWDogs = async (winner) => {
+//   const dogs = await fetchWDogs();
+// //   renderWDogs(dogs);
+// // };
+
+
+// // const renderWDogs = (dogs) => {
+//   let html2 = ``;
+//   let dogsVote = 0;
+//   for (const dog of dogs) {
+//     html2 += `<img id="M2" class="image" src= ${dog.url} alt="">`;
+//     if (dog.type === "dog") {
+//       dogsVote++;
+//     }
+//     let htmlM = ``;
+//     htmlM += `<div style="color:#168E22;font-size:50px">${dogsVote}</div>`;
+//     document.getElementById("Mdog").innerHTML = htmlM;
+//   }
+//   document.getElementById("sidebar_Mariana").innerHTML = html2;
 // };
-
-
-// const renderWDogs = (dogs) => {
-  let html2 = ``;
-  let dogsVote = 0;
-  for (const dog of dogs) {
-    html2 += `<img id="M2" class="image" src= ${dog.url} alt="">`;
-    if (dog.type === "dog") {
-      dogsVote++;
-    }
-    let htmlM = ``;
-    htmlM += `<div style="color:#168E22;font-size:50px">${dogsVote}</div>`;
-    document.getElementById("Mdog").innerHTML = htmlM;
-  }
-  document.getElementById("sidebar_Mariana").innerHTML = html2;
-};
-displayWDogs();
-//renderSidebar();
+// displayWDogs();
+renderSidebar();
